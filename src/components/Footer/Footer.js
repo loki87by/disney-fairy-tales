e8c992796da6ce77989613643750bec959903693
Alexey Akulich <loki1987by@yandex.ru>
2022-08-09 08:34:36 +0300

    add images & fix start components

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,37 +1,14 @@
 // **импорты
 import React from "react";
-import { TranslationContext } from "../../contexts/translationContext";
 import "./Footer.css";
-import "./styles/__text/Footer__text.css";
-import "./styles/__input/Footer__input.css";
 
 // **функционал
 function Footer() {
   // *состояния
-  const [colorValue, setColorValue] = React.useState("#000000");
-  const translation = React.useContext(TranslationContext);
-
-  // *получение нового состояния фона
-  function changeColorValue(e) {
-    setColorValue(e.target.value);
-  }
-
-  // *смена фона
-  React.useEffect(() => {
-    const html = document.querySelector("html");
-    html.style = `--bg-color: ${colorValue}`;
-  }, [colorValue]);
 
   // **DOM
   return (
     <footer className="Footer">
-      <p className="Footer__text">{translation.background} </p>
-      <input
-        type="color"
-        className="Footer__input"
-        defaultValue={colorValue}
-        onInput={changeColorValue}
-      ></input>
     </footer>
   );
 }
--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,78 +1,403 @@
-export const GAME = [
-  { number: "", area: "top1", back: "white", size: "10vmin", color: "black" },
-  { number: "", area: "top2", back: "white", size: "10vmin", color: "black" },
-  { number: "", area: "top3", back: "white", size: "10vmin", color: "black" },
-  { number: "", area: "top4", back: "white", size: "10vmin", color: "black" },
+import Dalmatines2 from "../assets/102-Dalmatians.mp3";
+import Alladin from "../assets/Alladin.mp3";
+import Alice from "../assets/Alice.mp3";
+import Aristocats from "../assets/Aristocats.mp3";
+import Bambi from "../assets/Bambi.mp3";
+import Bambi2 from "../assets/Bambi2.mp3";
+import BeautyBeast from "../assets/BeautyBeast.mp3";
+import Herocity from "../assets/Big-Hero-6.mp3";
+import BlackCaudron from "../assets/Black-Caudron.mp3";
+import Bolt from "../assets/Bolt.mp3";
+import Brave from "../assets/Brave.mp3";
+import BrothBear from "../assets/BrothBear.mp3";
+import BrotherBear2 from "../assets/Brother-Bear-2.mp3";
+import BugsLife from "../assets/BugsLife.mp3";
+import Cars from "../assets/Cars.mp3";
+import Cars2 from "../assets/Cars2.mp3";
+import Cars3 from "../assets/Cars3.mp3";
+import ChickenLittle from "../assets/Chicken-Little.mp3";
+import Cinderella from "../assets/Cinderella.mp3";
+import Coco from "../assets/Coco.mp3";
+import Dalmatins from "../assets/Dalmatins.mp3";
+import Dinosaurs from "../assets/Dinosaurs.mp3";
+import Dumbo from "../assets/Dumbo.mp3";
+import FindingDori from "../assets/FindingDori.mp3";
+import FoxHound from "../assets/Fox&Hound.mp3";
+import Frozen from "../assets/Frozen.mp3";
+import Frozen2 from "../assets/Frozen2.mp3";
+import GoDinosaurs from "../assets/GoDinosaurs.mp3";
+import GreatMouse from "../assets/GreatMouse.mp3";
+import Hercules from "../assets/Hercules.mp3";
+import HoRange from "../assets/Brave.mp3";
+import HunchbackNotredame from "../assets/HunchbackNotredame.mp3";
+import Incredibles from "../assets/Incredibles.mp3";
+import Incredibles2 from "../assets/Incredibles2.mp3";
+import InsideOut from "../assets/InsideOut.mp3";
+import JoongleBook from "../assets/JoongleBook.mp3";
+import JoongleBook2 from "../assets/JoongleBook2.mp3";
+import LadyTramp from "../assets/LadyTramp.mp3";
+import LiloStitch from "../assets/LiloStitch.mp3";
+import LionKing from "../assets/LionKing.mp3";
+import LionKing2 from "../assets/LionKing2.mp3";
+import Mermaid from "../assets/Mermaid.mp3";
+import Moana from "../assets/Moana.mp3";
+import Monsters from "../assets/Monsters.mp3";
+import MonstersUniversity from "../assets/MonstersUniversity.mp3";
+import Mulan from "../assets/Mulan.mp3";
+import Nemo from "../assets/Nemo.mp3";
+import OlivCo from "../assets/OlivCo.mp3";
+import PeterPan from "../assets/PeterPan.mp3";
+import Piglet from "../assets/Piglet.mp3";
+import Pinoccio from "../assets/Pinoccio.mp3";
+import Planes from "../assets/Planes.mp3";
+import Planes2 from "../assets/Planes2.mp3";
+import Pocahontas from "../assets/Pocahontas.mp3";
+import PrinceFrog from "../assets/PrinceFrog.mp3";
+import RalphInternet from "../assets/Ralph-Internet.mp3";
+import Ratatouille from "../assets/Ratatouille.mp3";
+import Rescuedounder from "../assets/Rescuedounder.mp3";
+import Rescuers from "../assets/Rescuers.mp3";
+import ReturnToNeverland from "../assets/Return-to-Neverland.mp3";
+import RobinHood from "../assets/RobinHood.mp3";
+import Robinson from "../assets/Robinson.mp3";
+import SleepingBeauty from "../assets/SleepingBeauty.mp3";
+import SwordStone from "../assets/SwordStone.mp3";
+import SnowWhite from "../assets/SnowWhite.mp3";
+import Tangled from "../assets/Tangled.mp3";
+import Tigger from "../assets/Tigger.mp3";
+import TinkerBell from "../assets/Tinker-Bell.mp3";
+import ToyStory1 from "../assets/ToyStory1.mp3";
+import ToyStory2 from "../assets/TStory2.mp3";
+import ToyStory3 from "../assets/Toy-Story-3.mp3";
+import ToyStory4 from "../assets/Toy-Story-4.mp3";
+import TreasurePlanet from "../assets/Treasure-Planet.mp3";
+import Twild from "../assets/Twild.mp3";
+import Up from "../assets/Up.mp3";
+import WalliE from "../assets/Walli-E.mp3";
+import WinnieAdventures from "../assets/WinnieAdventures.mp3";
+import WreckItRalph from "../assets/WreckItRalph.mp3";
+import Zootopia from "../assets/Zootopia.mp3";
+import Atlantis from "../assets/Atlantis.mp3";
+
+export const STORIES = [
   {
-    number: "",
-    area: "second1",
-    back: "white",
-    size: "10vmin",
-    color: "black",
+    file: Dalmatins,
+    name: "101 далматинец",
   },
   {
-    number: "",
-    area: "second2",
-    back: "white",
-    size: "10vmin",
-    color: "black",
+    file: Dalmatines2,
+    name: "102 Далматинца",
   },
   {
-    number: "",
-    area: "second3",
-    back: "white",
-    size: "10vmin",
-    color: "black",
+    file: Alladin,
+    name: "Алладин",
   },
   {
-    number: "",
-    area: "second4",
-    back: "white",
-    size: "10vmin",
-    color: "black",
-  },
-  { number: "", area: "third1", back: "white", size: "10vmin", color: "black" },
-  { number: "", area: "third2", back: "white", size: "10vmin", color: "black" },
-  { number: "", area: "third3", back: "white", size: "10vmin", color: "black" },
-  { number: "", area: "third4", back: "white", size: "10vmin", color: "black" },
-  {
-    number: "",
-    area: "bottom1",
-    back: "white",
-    size: "10vmin",
-    color: "black",
+    file: Alice,
+    name: "Алиса в стране чудес",
   },
   {
-    number: "",
-    area: "bottom2",
-    back: "white",
-    size: "10vmin",
-    color: "black",
+    file: Atlantis,
+    name: "Атлантида. Затерянный мир",
   },
   {
-    number: "",
-    area: "bottom3",
-    back: "white",
-    size: "10vmin",
-    color: "black",
+    file: SnowWhite,
+    name: "Белоснежка и 7 гномов",
   },
   {
-    number: "",
-    area: "bottom4",
-    back: "white",
-    size: "10vmin",
-    color: "black",
+    file: Twild,
+    name: "Большое путешествие",
+  },
+  {
+    file: Piglet,
+    name: "Большой фильм про поросенка",
+  },
+  {
+    file: BrothBear,
+    name: "Братец медвежонок",
+  },
+  {
+    file: BrotherBear2,
+    name: "Братец медвежонок 2",
+  },
+  {
+    file: Bambi,
+    name: "Бэмби",
+  },
+  {
+    file: Bambi2,
+    name: "Бэмби 2",
+  },
+  {
+    file: Robinson,
+    name: "В гости к Робинсонам",
+  },
+  {
+    file: FindingDori,
+    name: "В поисках Дори",
+  },
+  {
+    file: Nemo,
+    name: "В поисках Немо",
+  },
+  {
+    file: WalliE,
+    name: "Валл-И",
+  },
+  {
+    file: Up,
+    name: "Вверх",
+  },
+  {
+    file: GreatMouse,
+    name: "Великий мышиный сыщик",
+  },
+  {
+    file: Bolt,
+    name: "Вольт",
+  },
+  {
+    file: Hercules,
+    name: "Геркулес",
+  },
+  {
+    file: InsideOut,
+    name: "Головоломка",
+  },
+  {
+    file: HunchbackNotredame,
+    name: "Горбун из Нотр-Дамма",
+  },
+  {
+    file: Herocity,
+    name: "Город героев",
+  },
+  {
+    file: Dumbo,
+    name: "Дамбо",
+  },
+  {
+    file: Dinosaurs,
+    name: "Динозавр",
+  },
+  {
+    file: Zootopia,
+    name: "Зверополис",
+  },
+  {
+    file: Cinderella,
+    name: "Золушка",
+  },
+  {
+    file: ToyStory1,
+    name: "История игрушек",
+  },
+  {
+    file: ToyStory2,
+    name: "История игрушек 2",
+  },
+  {
+    file: ToyStory3,
+    name: "История игрушек 3",
+  },
+  {
+    file: ToyStory4,
+    name: "История игрушек 4",
+  },
+  {
+    file: JoongleBook,
+    name: "Книга джунглей",
+  },
+  {
+    file: JoongleBook2,
+    name: "Книга джунглей",
+  },
+  {
+    file: LionKing,
+    name: "Король-лев",
+  },
+  {
+    file: LionKing2,
+    name: "Король-лев 2",
+  },
+  {
+    file: Monsters,
+    name: "Корпорация монстров",
+  },
+  {
+    file: Aristocats,
+    name: "Коты аристократы",
+  },
+  {
+    file: BeautyBeast,
+    name: "Красавица и чудовище",
+  },
+  {
+    file: LadyTramp,
+    name: "Леди и Бродяга",
+  },
+  {
+    file: LiloStitch,
+    name: "Лило и Ститч",
+  },
+  {
+    file: FoxHound,
+    name: "Лис и пес",
+  },
+  {
+    file: SwordStone,
+    name: "Меч в камне",
+  },
+  {
+    file: Moana,
+    name: "Моана",
+  },
+  {
+    file: Mulan,
+    name: "Мулан",
+  },
+  {
+    file: HoRange,
+    name: "Не бей копытом",
+  },
+  {
+    file: OlivCo,
+    name: "Оливер и компания",
+  },
+  {
+    file: Pinoccio,
+    name: "Пиноккио",
+  },
+  {
+    file: PeterPan,
+    name: "Питер Пэн",
+  },
+  {
+    file: ReturnToNeverland,
+    name: "Питер Пэн: Возвращение в Нетландию",
+  },
+  {
+    file: TreasurePlanet,
+    name: "Планета сокровищ",
+  },
+  {
+    file: Pocahontas,
+    name: "Покахонтас",
+  },
+  {
+    file: WinnieAdventures,
+    name: "Приключения Винни",
+  },
+  {
+    file: Tigger,
+    name: "Приключения Тигрули",
+  },
+  {
+    file: BugsLife,
+    name: "Приключения флика",
+  },
+  {
+    file: PrinceFrog,
+    name: "Принцесса и лягушка",
+  },
+  {
+    file: WreckItRalph,
+    name: "Ральф",
+  },
+  {
+    file: RalphInternet,
+    name: "Ральф против интернета",
+  },
+  {
+    file: Tangled,
+    name: "Рапунцель",
+  },
+  {
+    file: Ratatouille,
+    name: "Рататуй",
+  },
+  {
+    file: RobinHood,
+    name: "Робин Гуд",
+  },
+  {
+    file: Mermaid,
+    name: "Русалочка",
+  },
+  {
+    file: Planes,
+    name: "Самолеты",
+  },
+  {
+    file: Planes2,
+    name: "Самолеты2",
+  },
+  {
+    file: Rescuers,
+    name: "Спасатели",
+  },
+  {
+    file: Rescuedounder,
+    name: "Спасатели в Австралии",
+  },
+  {
+    file: SleepingBeauty,
+    name: "Спящая красавица",
+  },
+  {
+    file: Incredibles,
+    name: "Суперсемейка",
+  },
+  {
+    file: Incredibles2,
+    name: "Суперсемейка 2",
+  },
+  {
+    file: Coco,
+    name: "Тайна Коко",
+  },
+  {
+    file: Cars,
+    name: "Тачки",
+  },
+  {
+    file: Cars2,
+    name: "Тачки 2",
+  },
+  {
+    file: Cars3,
+    name: "Тачки 3",
+  },
+  {
+    file: MonstersUniversity,
+    name: "Университет монстров",
+  },
+  {
+    file: TinkerBell,
+    name: "Феи",
+  },
+  {
+    file: Frozen,
+    name: "Холодное сердце",
+  },
+  {
+    file: Frozen2,
+    name: "Холодное сердце 2",
+  },
+  {
+    file: GoDinosaurs,
+    name: "Хороший динозавр",
+  },
+  {
+    file: Brave,
+    name: "Храбрая сердцем",
+  },
+  {
+    file: ChickenLittle,
+    name: "Цыпленок Цыпа",
+  },
+  {
+    file: BlackCaudron,
+    name: "Черный котел",
   },
 ];
-
-export const LUFT_KOEFFICIENT = 0.2;
-
-export const MAX_NUMBER = 2048;
-
-export const HALF_NUMBER = 1024;
-
-export const FIRST_COLOR_LEVEL = 16;
-
-export const SECOND_COLOR_LEVEL = 64;
-
-export const THIRD_COLOR_LEVEL = 128;
